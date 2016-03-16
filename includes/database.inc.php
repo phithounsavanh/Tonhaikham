be4593152ee81a0fae55b73da99a20b5c373e413
alex <Phithounsavanh@gmail.com>
2016-03-16 09:09:56 -0700

    First commit

---
+++ includes/database.inc.php
@@ -0,0 +1,6 @@
+<?php
+defined('DATABASE_HOST') ? NULL : define('DATABASE_HOST', 'localhost');
+defined('DATABASE_NAME') ? NULL : define('DATABASE_NAME', 'tonhaikham');
+defined('DATABASE_USER') ? NULL : define('DATABASE_USER', 'root');
+defined('DATABASE_PASSWORD') ? NULL : define('DATABASE_PASSWORD', '123456');
+?>
